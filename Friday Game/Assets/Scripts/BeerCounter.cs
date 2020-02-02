8677b3607729e4af473cc42b39e7b3160bb7ee39
AWieczarek <adamwie12@gmail.com>
2020-02-02 22:51:04 +0100

    Add Beers Counters and make some subfolders

---
+++ Friday Game/Assets/Scripts/BeerCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BeerCounter : MonoBehaviour
+{
+    public GameObject manager;
+    public TextMeshProUGUI beerCounter;
+
+    void Start()
+    {
+        beerCounter = GetComponent<TextMeshProUGUI>();
+        manager = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        beerCounter.text = manager.GetComponent<Beer>().NumberOfBeers.ToString();
+    }
+}
---
+++ Friday Game/Assets/Scripts/BeerZeroCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BeerZeroCounter : MonoBehaviour
+{
+    public GameObject manager;
+    public TextMeshProUGUI beerZeroCounter;
+
+    void Start()
+    {
+        beerZeroCounter = GetComponent<TextMeshProUGUI>();
+        manager = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        beerZeroCounter.text = manager.GetComponent<Beer>().NumberOfBeersZero.ToString();
+    }
+}
