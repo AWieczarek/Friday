46229d5866da0f6558b8993ed1865efd1c5c9302
AWieczarek <adamwie12@gmail.com>
2020-02-03 21:52:56 +0100

    Add shop template

---
+++ Friday Game/Assets/Scripts/Shop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject obj;
+    public bool shop = false;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if(shop)
+        {
+            obj.SetActive(true);
+        }
+        else
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    public void OpenShop(){
+        if(!shop)
+        {
+            shop = true;  
+		}
+        else
+        {
+            shop = false;  
+		}
+	}
+
+    public void Kupa()
+    {
+     Debug.Log("Kupa");
+	}
+}
